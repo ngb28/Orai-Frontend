27d64ea4d15a38e183a769d315d3b7e26909949d
Paritosh Gupta <paritosh.gupta@yahoo.co.uk>
2018-03-05 10:32:03 -0500

    fix: updated fonts and orai logo

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {Nav} from './nav';
-import logo from '../logo.svg';
+import logo from './logo.svg';
 import './header.css';
 
 export class Header extends React.Component {
